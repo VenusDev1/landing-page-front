47a8a88f2c65191334eb29da32e5242754382779
Dylan <dylanperaltasalerno@gmail.com>
2022-09-22 00:00:24 -0300

    visual fixes

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -2,13 +2,13 @@ import styles from '../../styles/Home.module.css'
 import Image from 'next/image'
 
 function About () {
-    return (<about className='about'>
+    return (<about className='w-[100%] h-[100vh] flex flex-row  '>
 
-                <div className='conteiner_about img_about'>
+                <div className='w-[40%] bg-gradient-to-r from-[#F5E4B1] flex justify-center items-center '>
                     <img src="img/about_img.png" alt="about_img"></img>
                 </div>  
 
-                <div className='conteiner_about'>
+                <div className='w-[60%] flex flex-col p-10 '>
                     <div className='text_about'>
                         <h1>About VENUS Dev</h1>
                         <h2>Somos una startup enfocada en desarrollo web, nuestro objetivo es ayudarte a dar el siguiente paso hacia la transformación digital. A travez de nuestros servicios de desarrollo y marketing llevaremos tu negocio a internet. </h2>
--- components/Layouts/BaseLayout.jsx
+++ components/Layouts/BaseLayout.jsx
@@ -4,7 +4,7 @@ const BaseLayout = ({ children, className }) => {
      return (
         <>
         <Header />
-        <div className="flex flex-col w-[100vw] ">
+        <div className="flex flex-col w-[100%] ">
           <main className="w-[100%] mx-auto relative flex flex-col flex-1 h-screen overflow-y-auto">
             {children}
           </main>
