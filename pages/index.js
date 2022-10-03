2af800687df0478a64cad53f60f11418c5b730ca
Dylan <dylanperaltasalerno@gmail.com>
2022-10-02 22:33:47 -0300

    fixes v2

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,8 @@ import Navbar from '../components/Navbar/Navbar'
 import Hero from '../components/Hero/Hero'
 import About from '../components/About/About'
 import Services from '../components/Services/Services'
-import Contact from '../components/Contact/Contact'
 import ContactPages from '../components/ContactPages/ContactPages';
 import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
 import BaseLayout from '../components/Layouts/BaseLayout'
 
 function Index() {
@@ -15,10 +13,10 @@ function Index() {
       <Hero/>
       <About/>
       <Services/>
-      <div className='w-[100%] h-[100px] lg:h-[150px] bg-black ' />
       <ContactPages/>
       <Footer/>
-    </BaseLayout>)
+    </BaseLayout>
+  )
 }
 
 export default Index;
